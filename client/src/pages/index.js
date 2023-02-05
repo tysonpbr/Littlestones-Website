9b71ce72badb8ccb8e9d0796a619633ec2ae9c49
Tyson Brown <90238566+tysonpbr@users.noreply.github.com>
2023-02-05 10:04:17 -0800

    first commit

---
+++ client/src/pages/index.js
@@ -0,0 +1,13 @@
+import About from './About';
+import Contact from './Contact';
+import Gallery from './Gallery';
+import Home from './Home';
+import Services from './Services';
+
+export {
+  About,
+  Contact,
+  Gallery,
+  Home,
+  Services
+}
